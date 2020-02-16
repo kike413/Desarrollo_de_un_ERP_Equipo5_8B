6e735314ac6c3aaafef618a143a088a51a060582
cbgama <egama350@accitesz..com>
2020-02-15 18:12:16 -0600

    Cambio esquema de proyecto a MVC

---
+++ Datos/ConexionSQL.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+namespace Datos
+{
+        public abstract class ConexionSQL
+    {
+        private readonly string connectionString;
+        public ConexionSQL()
+        {
+            connectionString = "Server=DESKTOP-F7OUO59;DataBase=ERP2020; integrated security = true";
+        }
+        protected SqlConnection GetConnection()
+        {
+            return new SqlConnection(connectionString);
+        }
+    }
+}
---
+++ Datos/UsuarioDAO.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+namespace Datos
+{
+    public class UsuarioDAO:ConexionSQL //herencia de conexionsql
+    {
+        /**
+         * Método para verificar el login
+         */
+        public bool Login(string usuario,string password)
+        {
+            using (var connection = GetConnection()){
+                connection.Open();
+                using (var command = new SqlCommand())
+                {
+                    command.Connection = connection;
+                    command.CommandText = "select * from usuarios where nombre=@usuario and" +
+                        " contraseña=@password";
+                    command.Parameters.AddWithValue("@usuario",usuario);
+                    command.Parameters.AddWithValue("@password", password);
+                    command.CommandType = CommandType.Text;
+                    SqlDataReader reader = command.ExecuteReader();
+                    if (reader.HasRows)
+                    {
+                        return true;
+                    }
+                    else
+                        return false;
+                }
+            }
+        }
+    }
+}
