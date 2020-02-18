cc82e34410fe0e89dcfcf16f7f796abab3980d53
Pablo354 <60712814+Pablo354@users.noreply.github.com>
2020-02-18 07:59:16 -0600

    Add files via upload

---
+++ Datos/PrincipalDAO.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos
+{
+    public class PrincipalDAO:ConexionSQL
+    {
+        SqlDataReader leer;
+
+
+        /*
+        * Selecciona el nombre del usuario 
+        */
+        public void SeleccionarNombre(string nombre)
+        {
+            using (var connection = GetConnection())
+            {
+                connection.Open();
+                using (var command = new SqlCommand())
+                {
+                    command.Connection = connection;
+                    command.CommandText = "SeleccionarNombreUsuario";
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@nombre", nombre);
+                    command.ExecuteNonQuery();
+                    command.Parameters.Clear();
+                    connection.Close();
+                }
+            }
+        }
+
+    }
+}
