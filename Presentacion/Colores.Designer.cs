89197a7ce697bae0e15ff9fcaa5ede9a862b61f9
Jorge Enrique Vega Mejia <54999823+kike413@users.noreply.github.com>
2020-02-17 19:38:51 -0600

    GUI Coloes

---
+++ Presentacion/Colores.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace Presentacion
+{
+    partial class Colores
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(215, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Selecciona un Color:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(19, 268);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 53);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Acceptar ";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(130, 268);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(98, 53);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Canselar";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(19, 47);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(584, 214);
+            this.listView1.TabIndex = 3;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(609, 47);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 56);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Agregar Un Nuevo Color";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(609, 109);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(100, 56);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Eliminar Color Seleccionado";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // Colores
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(721, 353);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Colores";
+            this.Text = "Colores";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
