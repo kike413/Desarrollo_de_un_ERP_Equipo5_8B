6ae0195b4cf8fc5aad0392c6a10fbd93f08bfc27
Jorge Enrique Vega Mejia <54999823+kike413@users.noreply.github.com>
2020-02-18 05:30:54 -0600

    CRUD - Colores

---
+++ Dominio/Colores.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using Datos;
+
+namespace Dominio
+{
+    public class Colores
+    {
+        private ColoresDAO coloresDao = new ColoresDAO();
+
+        public DataTable MostrarColores()
+        {
+            DataTable tabla = new DataTable();
+            tabla = coloresDao.Mostrar();
+            return tabla;
+        }
+
+        public void InsertarColor(string nombre)
+        {
+            coloresDao.Insertar(nombre);
+        }
+
+        public void EditarColor(string nombre,string id)
+        {
+            coloresDao.Editar(nombre, Convert.ToInt32(id));
+        }
+
+        public void EliminarColor(string id)
+        {
+            coloresDao.Eliminar(Convert.ToInt32(id));
+        }
+
+    }
+}
