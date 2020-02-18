33c3564123c5d323119530013bc056ee33fd6521
Pablo354 <60712814+Pablo354@users.noreply.github.com>
2020-02-18 11:15:23 -0600

    Add files via upload

---
+++ Dominio/Categorias.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Datos;
+
+namespace Dominio
+{
+    class Categorias
+    {
+        private CategoriasDAO categoriasdao = new CategoriasDAO();
+        public void seleccionNombre(string nombre)
+        {
+            //categoriasdao.SeleccionarNombre(nombre);
+        }
+
+        public void seleccionID(int id)
+        {
+            //categoriasdao.SeleccionarID(id);
+        }
+    }
+}
