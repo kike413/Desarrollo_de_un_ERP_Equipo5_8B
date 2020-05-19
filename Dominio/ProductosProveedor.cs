0b1038035b5ff6f6d3baaa446063430b57ee3056
Pablo354 <60712814+Pablo354@users.noreply.github.com>
2020-05-19 10:52:32 -0500

    Add files via upload
    Archivo Productos Proveedor añadido

---
+++ Dominio/ProductosProveedor.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using Datos;
+
+namespace Dominio
+{
+    public class ProductosProveedor
+    {
+        private ProductosProveedorDAO PPDAO = new ProductosProveedorDAO();
+        
+        public DataTable MostrarProductosProveedorDAO(int pagina)
+        {
+            DataTable tabla = new DataTable();
+            tabla = PPDAO.Mostrar(pagina);
+            return tabla;
+        }
+
+        public void InsertarProductosProveedor(string diasRetardo, string precioEstandar, string precioUltimaCompra, string cantMinPedir, string cantMaxPedir)
+        {//Convert.ToString()
+            PPDAO.Insertar(Convert.ToInt32(diasRetardo), Convert.ToSingle(precioEstandar), Convert.ToSingle(precioUltimaCompra), Convert.ToInt32(cantMinPedir), Convert.ToInt32(cantMaxPedir));
+        }
+
+        public void EditarProductosProveedor (string diasRetardo, string precioEstandar, string precioUltimaCompra, string cantMinPedir, string cantMaxPedir, string id)
+        {
+            PPDAO.Editar(Convert.ToInt32(diasRetardo), Convert.ToSingle(precioEstandar), Convert.ToSingle(precioUltimaCompra), Convert.ToInt32(cantMinPedir), cantMaxPedir, Convert.ToInt32(id));
+        }
+
+        public void EliminarProductosProveedor(string id)
+        {
+            PPDAO.Eliminar(Convert.ToInt32(id));
+        }
+
+        public int obtenerPaginas()
+        {
+            int pags = PPDAO.obtenerPaginas();
+            return pags;
+        }
+        /**public int obtenerIdColor(string campo)
+        {
+            int ids = DPDAO.obtenerIdColor(campo);
+            return ids;
+        }**/
+
+        public int obtenerIdProducto(string campo)
+        {
+            int ids = PPDAO.obtenerIdProducto(campo);
+            return ids;
+        }
+    }
+}
